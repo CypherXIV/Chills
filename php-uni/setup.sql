faee9fc4f2027e60842523f9067e16089c36df38
CypherXIV <101220011+CypherXIV@users.noreply.github.com>
2023-11-01 21:59:27 -0400

    Delete php-uni directory

--- php-uni/setup.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE php_uni_db;
-
-USE php_uni_db;
-
-CREATE TABLE users (
-    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
